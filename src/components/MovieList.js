339ed5f30edbf44684457297945e4ee4c394cb5f
Ronaemre <ronaemre7231@gmail.com>
2021-08-19 15:36:36 +0300

    ROuter Added

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,10 +1,7 @@
 import React from "react";
 
 const MovieList = (props) => {
-  /*   function handleClick(e) {
-    console.log(e.pageX);
-  }
- */
+
   return (
     <div className="row">
       {props.movies.map((movie) => (
@@ -38,34 +35,6 @@ const MovieList = (props) => {
   );
 };
 
-/*   render() {
-    return (
-      <div className="row">
-        <div className="col-lg-4">
-          <div className="card mb-4 shadow-sm">
-            <img
-              src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/qHCZ6LjtmqWDfXXN28TlIC9OppK.jpg"
-              className="card-img-top"
-              alt="Sample Movie"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Sample Movie</h5>
-              <p className="card-text"></p>
-              <div className="d-flex justify-content-between align-items-center">
-                <button type="button" className="btn btn-md btn-outline-danger">
-                  Delete
-                </button>
 
-                <h2>
-                  <span className="badge badge-info">9.0</span>
-                </h2>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-} */
 
 export default MovieList;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,13 +13,22 @@ class SearchBar extends React.Component {
     return (
       <form onSubmit={this.handleFormSubmit}>
         <div className="form-row mb-5">
-          <div className="col-12">
+          <div className="col-10">
             <input
               onChange={this.props.searchMovieProp}
               type="text"
               className="form-control"
               placeholder="Search a movie"
             />
+          </div>
+          <div className="col-2">
+            <button
+              type="button"
+              className="btn btn-md btn-danger"
+              style={{ float: "right" }}
+            >
+              Add Movie
+            </button>
           </div>
         </div>
       </form>
